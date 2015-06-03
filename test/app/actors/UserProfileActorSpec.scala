549ae9b215fcb7e0888fe1ecc4416b6121cdfdc7
Krisztian Lachata <krisztian.lachata@gmail.com>
2015-06-03 22:30:32 +0100

    new test for user profile actor

--- test/app/actors/UserProfileActorSpec.scala
+++ test/app/actors/UserProfileActorSpec.scala
@@ -2,14 +2,17 @@ package app.actors
 
 import actors.UserProfileActor
 import akka.actor.ActorSystem
+import akka.pattern.ask
 import akka.testkit.{ImplicitSender, TestActorRef, TestKit}
 import akka.util.Timeout
 import org.mockito.Mockito._
+import org.scalatest.Matchers._
 import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
 import twitter4j._
 
+import scala.collection.JavaConversions._
+import scala.concurrent.Await
 import scala.concurrent.duration._
-
 
 class UserProfileActorSpec extends TestKit(ActorSystem("UserProfileActorSpec"))
 with WordSpecLike
@@ -22,16 +25,13 @@ with ImplicitSender {
 
   "UserProfileActor" should {
 
-    //TODO test this function
-    //    "send filter list to twitter actor if valid JsValue arrives" in new scope {
-    //
-    //      val result = for {
-    //        user <- (userProfileActor ? 10L).mapTo[User]
-    //      } yield user
-    //
-    //      println(result)
-    //      expectMsg(result)
-    //    }
+    "send back requested user profile to the sender" in new scope {
+
+      val result = Await.result((userProfileActor ? 10L).mapTo[User], 5 seconds)
+
+      result should equal(user)
+      verify(twitterInstance).lookupUsers(Array(10L))
+    }
 
   }
 
@@ -42,7 +42,7 @@ with ImplicitSender {
     val responseList = mock(classOf[ResponseList[User]])
     val user = mock(classOf[User])
 
-    when(responseList.get(0)).thenReturn(user)
+    when(responseList.iterator()).thenReturn(List(user).iterator)
     when(twitterInstance.lookupUsers(Array(10L))).thenReturn(responseList)
 
     val userProfileActor = TestActorRef(new UserProfileActor(twitterInstance))
